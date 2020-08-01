c9f0782a556a312dc248557814267c9eb8df0788
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-08-01 11:53:45 -0700

    hotfix -cors

--- api/server.js
+++ api/server.js
@@ -6,14 +6,19 @@ const templateRoute = require('../routes/template');
 //Require env variables
 require('dotenv').config();
 const server = express();
+
 server.use(helmet());
+server.use(express.json());
+
 server.use(cors({
   origin: [
     'http://localhost:3000',
     'https://labs20d8picker.netlify.app'
-  ]
+  ],
+  credentials: true
 }));
-server.use(express.json());
+
+
 
 server.use('/api/template', templateRoute);
 
